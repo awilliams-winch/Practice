b3a1da4567c0f3e91aa84493e2e80fa8334b7442
awilliams-winch <awilliams@winchendon.org>
2024-10-02 10:37:25 -0400

    broken_program_1.py added

---
+++ broken_program_1.py
@@ -0,0 +1,43 @@
+# 
+# The following program is very buggy! It does not have the desired output.
+# Can you identify the errors in the program and fix them?
+#
+
+
+# Part 1: A Broken Madlib
+
+# The following lines should take input from the user and assign them to variables
+place = input("Albania")
+adjective = "Give me an adjective"
+emotion <= input("Give me an emotion")
+ing_verb = input("doing"")
+noun = noun
+
+# The following should concatenate the variables above into lines in the Madlib
+line1 = "Today, I went to <place>. It was very " adjective_1
+line 2 = "When I arrived, I felt" + emotion
+line3 = "I spent the day " (ing_verb) " to the " + noun
+
+# Then, print the lines of the Madlib
+print(line1 line2 line_3) 
+
+
+
+
+# Part 2: A broken math problem
+
+# The following lines should take two inputs from the user and convert them to ints
+int_1 = str(input('Give me an integer please'))
+int_2 = int(input('Give me another integer please')
+
+#Take the two inputs and perform basic operations on them together
+sum = int1 + int_2
+difference = int_1 -- int_2
+product = int_1 ** int_2
+quotient = int_1 / int_2
+
+#Print the answer as integers
+print(sum)
+print difference
+print("product")
+print(quotient)
